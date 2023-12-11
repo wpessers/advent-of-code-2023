87168db938864770388356b2f5bdec01f63691cd
warre <warre.pessers@gmail.com>
2023-12-11 13:50:52 +0100

    Solve day 7 - part 1

---
+++ day07/src/bin/part1.rs
@@ -0,0 +1,78 @@
+use itertools::Itertools;
+use std::ops::Deref;
+
+#[derive(Debug, Clone, Copy)]
+enum Hand {
+    FiveOfAKind = 6,
+    FourOfAKind = 5,
+    FullHouse = 4,
+    ThreeOfAKind = 3,
+    TwoPair = 2,
+    OnePair = 1,
+    HighCard = 0,
+}
+
+fn calculate_score(hand: &str) -> (Hand, (u32, u32, u32, u32, u32)) {
+    use Hand::*;
+
+    let counts = hand.chars().counts();
+    let values = counts.values().sorted().join("");
+    let hand_type = match values.deref() {
+        "5" => FiveOfAKind,
+        "14" => FourOfAKind,
+        "23" => FullHouse,
+        "113" => ThreeOfAKind,
+        "122" => TwoPair,
+        "1112" => OnePair,
+        "11111" => HighCard,
+        value => panic!("Should not occur, found {}", value),
+    };
+    let card_scores = hand
+        .chars()
+        .map(|card| match card {
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'J' => 11,
+            'T' => 10,
+            value => value.to_digit(10).unwrap(),
+        })
+        .collect_tuple()
+        .unwrap();
+    (hand_type, card_scores)
+}
+
+fn part1(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let (hand, bid) = line.split_once(' ').unwrap();
+            (hand, bid.parse::<u32>().unwrap(), calculate_score(hand))
+        })
+        .sorted_by_key(|x| (x.2 .0 as u8, x.2 .1))
+        .enumerate()
+        .map(|(index, (_hand, bid, _))| (index as u32 + 1) * bid)
+        .sum::<u32>()
+        .to_string()
+}
+
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    println!("{output}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+        assert_eq!("288", part1(input));
+    }
+}
